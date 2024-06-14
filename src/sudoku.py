aeb9e55d908be7b6125b106fd27b6358ab58f0ec
mier <mier@tanglewreck.mier.org>
2024-06-14 15:10:28 +0200

    Trying stuff...

--- src/sudoku.py
+++ src/sudoku.py
@@ -13,7 +13,8 @@ DEBUG = False
 def read_game_file(filename=None, debug=DEBUG):
     rows = []
     try:
-        with open("../dn_2024-06-11_hard.sudoku", 'r') as f:
+        # with open("../dn_2024-06-11_hard.sudoku", 'r') as f:
+        with open(filename, 'r') as f:
             while row := f.readline():
                 nrow = list(map(int, re.findall(r"\d+", row)))
                 rows.append(nrow)
@@ -22,14 +23,47 @@ def read_game_file(filename=None, debug=DEBUG):
                     debug_msg(row)
     except OSError as e:
         err_msg(e)
-    return rows
+    return np.array(rows)
 
+
+def print_board(board):
+    print(" – – – – – – – – – – – –")
+    for i, row in enumerate(board):
+        # print(row)
+        s = "| "
+        for j, element in enumerate(row):
+            s += str(element) + " "
+            if (j + 1) % 3 == 0:
+                s += "| "
+        print(s)
+        if (i + 1) % 3 == 0:
+            print(" – – – – – – – – – – – –")
 
 def main():
     args = parse_arguments()
-    rows = read_game_file(args.filename, debug=args.debug)
+    board = read_game_file(args.filename, debug=args.debug)
 
-    print(rows)
+    blocks = []
+    blocks.append(board[0:3, 0:3])
+    blocks.append(board[0:3, 3:6])
+    blocks.append(board[0:3, 6:9])
+
+    blocks.append(board[3:6, 0:3])
+    blocks.append(board[3:6, 3:6])
+    blocks.append(board[3:6, 6:9])
+
+    blocks.append(board[6:9, 0:3])
+    blocks.append(board[6:9, 3:6])
+    blocks.append(board[6:9, 6:9])
+
+    for k, block in enumerate(blocks): 
+        # print(block, end='    ')
+        if k % 3 == 0:
+            print()
+        print(block)
+
+
+
 
 if __name__ == "__main__":
     main()
