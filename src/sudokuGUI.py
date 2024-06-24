2e4aa828e824039953a1a1a868fe598074476aaa
mier <mier@tanglewreck.mier.org>
2024-06-24 12:51:44 +0200

    Clean-up: renamed variables for clarity...

--- src/sudokuGUI.py
+++ src/sudokuGUI.py
@@ -17,29 +17,29 @@ from sudokuboard import Sudoku
 class SudokuGUI():
     """A class representing the graphical interface of a sudoku game"""
 
-    def __init__(self, sudoku_game:Sudoku, geometry:str=ROOT_GEOMETRY, debug=False):
+    def __init__(self, sudoku:Sudoku, geometry:str=ROOT_GEOMETRY, debug=False):
         self.debug = debug
-        self.widgets = SudokuWidgets(sudoku_game=sudoku_game, geometry=geometry, debug=self.debug)
-        self.sudoku = sudoku_game
-        self.board = sudoku_game.board
+        self.widgets = SudokuWidgets(geometry=geometry, debug=self.debug)
+        self.sudoku = sudoku
+        self.board = self.sudoku.board
 
 
     def mainloop(self):
         self.widgets.root.mainloop()
 
 
-    def populate(self, sudoku_game, debug=False):
+    def populate(self, sudoku):
         """Populate the content frame with numbers"""
 
-        if debug:
-            debug_msg(f"\n{self.board}")
+        if self.debug:
+            debug_msg(f"\n{self.sudoku.board}")
 
         # Create a list of lists of Sudoku number labels
         L = list()
         for i in range(9):
             l = list()
             for j in range(9):
-                l.append(ttk.Label(self.widgets.content, text=f"{self.board[i][j]}"))
+                l.append(ttk.Label(self.widgets.content, text=f"{self.sudoku.board[i][j]}"))
             L.append(l)
 
         # Place the number labels using grid())
