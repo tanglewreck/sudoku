9638e0d92628f36caa76460dca8985ab4f5b0191
mier <mier@tanglewreck.mier.org>
2024-08-07 12:44:35 +0200

    –General cleanup: lots of commented code removed, and more – BoardButton now inherits NumberPadButton, thereby re-using canvas definitions etc – Implemented a do_update() method that updates the active_number (NumberPadButton) and button text (BoardButton), respectively – Experimented with updating button texts *and* the board (numpy) array: maybe this is the way to do stuff, after all, disregardning that each BoardButton must do separate, individual calls to the (root) update function...?

--- src_kivy/main.py
+++ src_kivy/main.py
@@ -38,6 +38,7 @@ from kivy.core.window import Window
 from kivy.properties import ColorProperty
 from kivy.properties import ListProperty
 from kivy.properties import NumericProperty
+from kivy.properties import ObjectProperty
 from kivy.properties import StringProperty
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
@@ -47,12 +48,16 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
 import numpy as np
+import string
 import sys
 
 from utils import debug_msg, err_msg, sys_msg
 from utils import parse_arguments
 from utils import WINDOW_SIZE
 
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# <KIVY CONFIG>
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 # Read the (local) configuration file, change stuff to our liking, then write it.
 Config.read(f"{HOME}/Proj/sudoku/src_kivy/.kivy/config.ini")
 Config.set('kivy', 'exit_on_escape', 1)
@@ -68,10 +73,14 @@ Config.set('graphics', 'top', 100)          # ignored when 'position' == 'auto'
 # log_level := 'debug'|'info'|'warning'|'error'|'critical'
 Config.set('kivy', 'log_level', "warning")
 Config.write()
+# Re-read the config (restart is needed to change the  window_icon):
 # Config.read(f"{HOME}/Proj/sudoku/src_kivy/.kivy/config.ini")
 
 # Config.set('modules', 'monitor', '')
 # Config.set('modules', 'touchring', '')
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# </KIVY CONFIG>
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 
 # The size of the root window can be set thus:
@@ -81,29 +90,38 @@ Config.write()
 
 
 class NumberPadButton(Button):
+
     def __init__(self, **kwargs):
         super(NumberPadButton, self).__init__(**kwargs)
 
+    def do_update(self, text):
+        # Set the text of the Label:
+        #                   <BoxLayoyt>
+        #                   |      <AnchorLayoyt>
+        #                   |      |
+        self.r00t= self.parent.parent
+        if text in string.digits:
+            self.r00t.children[2].children[0].text = text
+        else:
+            self.r00t.children[2].children[0].text = ""
 
-class BoardButton(Button):
+
+
+class BoardButton(NumberPadButton):
+
     def __init__(self, **kwargs):
         super(BoardButton, self).__init__(**kwargs)
-    nn = StringProperty("1")
-    pass
-#    def __init__(self, **kwargs):
-#        super(BoardButton, self).__init__(**kwargs)
-#     def update_block_button(self, text):
-#         #
-#         # This is butt ugly! Gotta find a cleaner, neater, better way of
-#         # updating the blockbutton text!
-#         #
-#         #           <SudokuBlock>
-#         #                |      <GridLayout>
-#         #                |      |      <RootWidget>
-#         #                |      |      |      <BoxLayout>
-#         #                |      |      |      |           <Label>
-#         #                |      |      |      |           |
-#         self.text = self.parent.parent.parent.children[0].children[1].text # the text property of the Label widget (i.e. active_number)
+
+    def do_update(self, text):
+        # Set the text to the text of the Label:
+        #           <SudokuBlock>
+        #                |      <GridLayout>
+        #                |      |      <RootWidget>
+        #                |      |      |      <BoxLayout>
+        #                |      |      |      |           <Label>
+        #                |      |      |      |           |
+        self.r00t= self.parent.parent.parent.children
+        self.text = self.r00t[0].children[2].children[0].text
 
 
 class SudokuBlock(GridLayout):
@@ -119,67 +137,54 @@ class SudokuBlockLight(SudokuBlock):
 # RootWidget: The root of our widget tree: a sudoku board
 #             with a numerical keypad on the right
 class RootWidget(BoxLayout):
-    nnn = StringProperty("2")
-    lll = ListProperty()
-
-    Active_Number = StringProperty("spam")
-    print(Active_Number)
-    N = NumericProperty(100)
-    print(N)
-    #if N:
-    #    print("100 xyz")
-    #    print(N)
-
-    def __init__(self, board=None,
-                 debug=None, **kwargs):
+    # note = False
+    def __init__(self, board=None, debug=None, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
+
         self.board = board
         if not self.board.all():
-            sys_msg("non-zero b")
-            print(board)
-            for i in range(9):
-                self.lll.append(list(self.board[i]))
-            for i in range(9):
-                for j in range(9):
-                    print(self.lll[i][j], end="")
-                print()
-            # print(" ".join(self.lll[0]))
-            # self.lll = self.board
-            #self.lll = "apa"
-            l = list()
-            self.ids['s11'].text = str(self.board[0][0])
-            self.ids['s12'].text = str(self.board[0][1])
-            self.ids['s13'].text = str(self.board[0][2])
-            for k in range(9):
-                for l in range(9):
-                    id = f"s{k}{l}"
-                    if self.board[k,l]:
-                        # self.ids["s" + str(k) + str(l)].text = str(self.board[k,l])
-                        self.ids[id].text = str(self.board[k,l])
-                        self.ids[id].disabled = True
-                    else:
-                        # self.ids["s" + str(k) + str(l)].text = ""
-                        self.ids[id].text = ""
-
-
+            print(self.board)
+            self.populate_board()
             
-        # pass
-    def f_s11(self, n):
-        n = int(n)
-        print(n + 1)
-        # print(self.thenumber.text)
-        # self.s11 = self.thenumber.text
-        self.s11x.text = str(n + 1)
-        print(self.block11.text)
-        print(self.ids['s12'].text)
+    def populate_board(self):
+        for k in range(9):
+            for l in range(9):
+                id = f"s{k}{l}"
+                if self.board[k,l]:
+                    # self.ids["s" + str(k) + str(l)].text = str(self.board[k,l])
+                    self.ids[id].text = str(self.board[k,l])
+                    self.ids[id].disabled = True
+                else:
+                    # self.ids["s" + str(k) + str(l)].text = ""
+                    self.ids[id].text = ""
 
     def do_quit(self, *args):
         print("See ya!", file=sys.stderr)
         raise SystemExit(0)
 
-    def foo(self):
+    def foo(self, id=None, index=None):
         print("fubar")
-        print(type(self))
+        if self.ids['active_number'].text:
+            self.board[index] = int(self.ids['active_number'].text)
+        print(index)
+        print(self.board[index])
+        # print(self.board[index])
+        # self.ids['s05'].text = "99"
+        # print(type(self))
+
+    # def root_do_update(self, text):
+        # Set the text of the Label:
+        #                   <BoxLayoyt>
+        #                   |      <AnchorLayoyt>
+        #                   |      |
+        # self.r00t= self.parent.parent
+        # if text in string.digits:
+        #     self.r00t.children[2].children[0].text = text
+        # else:
+        #    self.r00t.children[2].children[0].text = ""
+        # print("root_do_update")
+
+
 
 
 class SudokuApp(App):
@@ -188,22 +193,10 @@ class SudokuApp(App):
         super(SudokuApp, self).__init__(**kwargs)
         self.debug = debug # gets passed on to RootWidget in self.build() below
         self.board = board # gets passed on to RootWidget in self.build() below
-        #if not self.board.all():
-        #    print("The board:")
-        #    print(self.board)
-        #else:
-        #    print(self.board)
-        #
-        # Which .kv file to use:
-        #
-        # self.kv_directory = "/Users/mier/Proj/sudoku/src_kivy/kv/"
-        # self.kv_file = "/Users/mier/Proj/sudoku/sudoku.kv"
-        # self.kv_file = "/Users/mier/Proj/sudoku/src_kivy/sudoku.kv"
         self.kv_file = "kv/sudoku.kv"
 
     def build(self):
         self.root = RootWidget(self.board, self.debug)
-        # self.icon = 'mier_347x437.jpg' # Use our own window icon
         return self.root
 
     def on_start(self):
