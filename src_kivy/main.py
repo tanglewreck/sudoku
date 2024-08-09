65d9b618a7aa567045d9c49ba05f3da0007c591b
mier <mier@tanglewreck.mier.org>
2024-08-09 14:09:20 +0200

    Lots of experimentation with defining widgets in the python code and the kv code, respectively. Lots of commented code that should, eventually be removed. Also experimented with combining GridLayouts with ButtonBehavior (to be able to make notes in really tiny fontsize inside a button), but No real progress on that. To be continued...

--- src_kivy/main.py
+++ src_kivy/main.py
@@ -14,7 +14,7 @@ import os
 # thereby breaking the PEP8 recommendation to put
 # module level imports at the top (E402):
 os.environ['KIVY_HOME'] = "./.kivy"
-HOME = os.environ['HOME']
+HOME = os.environ['HOME']  # Needed for the path to the kivy config file
 
 # Make kivy ignore command-line arguments so that we don't have to put '--'
 # before our own options/arguments
@@ -35,17 +35,19 @@ kivy.require("2.0.0")
 from kivy.app import App
 from kivy.config import Config
 from kivy.core.window import Window
+from kivy.graphics import Color, Rectangle
 from kivy.properties import ColorProperty
 from kivy.properties import ListProperty
 from kivy.properties import NumericProperty
 from kivy.properties import ObjectProperty
 from kivy.properties import StringProperty
+from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
 
 import functools
 import numpy as np
@@ -59,23 +61,23 @@ from utils import WINDOW_SIZE
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 # <KIVY CONFIG>
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-# Read the (local) configuration file, change stuff to our liking, then write it.
+# Read the (local) configuration files and change stuff to our liking.
+# NOTE: Changes require a restart to take effect
 Config.read(f"{HOME}/Proj/sudoku/src_kivy/.kivy/config.ini")
 Config.set('kivy', 'exit_on_escape', 1)
 Config.set('kivy', 'log_enable', 1)
 Config.set('kivy', 'window_icon', "data/mier_347x437.jpg")
 Config.set('graphics', 'fullscreen', 0)
-Config.set('graphics', 'height', 600)
-Config.set('graphics', 'width', 1000)
+# Config.set('graphics', 'height', 600)
+# Config.set('graphics', 'width', 1000)
+Config.set('graphics', 'height', 480)
+Config.set('graphics', 'width', 800)
 Config.set('graphics', 'resizable', 1)
-Config.set('graphics', 'position', 'auto')
-Config.set('graphics', 'left', 500)         # ignored when 'position' == 'auto'
+Config.set('graphics', 'position', 'custom')  # auto|custom
+Config.set('graphics', 'left', 100)         # ignored when 'position' == 'auto'
 Config.set('graphics', 'top', 100)          # ignored when 'position' == 'auto'
-# log_level := 'debug'|'info'|'warning'|'error'|'critical'
-Config.set('kivy', 'log_level', "warning")
+Config.set('kivy', 'log_level', "warning")  # debug|info|warning|error|critical
 Config.write()
-# Re-read the config (restart is needed to change the  window_icon):
-# Config.read(f"{HOME}/Proj/sudoku/src_kivy/.kivy/config.ini")
 
 # Config.set('modules', 'monitor', '')
 # Config.set('modules', 'touchring', '')
@@ -84,41 +86,96 @@ Config.write()
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 
-# The size of the root window can be set thus:
-# Window.size = (1000, 600)
-# or thus (using 'WINDOW_SIZE' set in utils.py):
+# The size of the root window is now set in the config file:
 # Window.size = WINDOW_SIZE
 
 
+class ButtonBG(Widget):
+    def __init__(self):
+        super(ButtonBG, self).__init__()
+        with self.canvas.before:
+            Color(1, 1, 1, 1)
+            Rectangle(pos=self.pos, size=self.size)
+        with self.canvas:
+            Color(1, 1, 1, 0.3)
+            Rectangle(pos=self.pos, size=self.size)
+        self.background_color = (1, 1, 1, 1)
+
+
+# Number pad buttons
 class NumberPadButton(Button):
-    pass
+    def __init__(self, **kwargs):
+        super(NumberPadButton, self).__init__(**kwargs)
+#    pass
 
 
+# Sudoku board buttons
 class BoardButton(Button):
     pass
+#    def __init__(self, **kwargs):
+#        super(BoardButton, self).__init__(**kwargs)
+#        with self.canvas.before:
+#            Color(1, 1, 1, 0)
+#            Rectangle(pos=self.pos, size=self.size)
+#        with self.canvas.after:
+#            Color(1, 1, 1, 0)
+#            Rectangle(pos=self.pos, size=self.size)
+#        self.color = (1, 1, 1, 1)
+#        self.background_color = (1, 1, 1, 1)
+#        self.disabled_color = (0.85, 0.85, 0.85, 1)
+#        self.font_size = '18sp'
+#        self.opacity = 1
+#        self.background_disabled_normal = "atlas://data/images/defaulttheme/button"
+#        self.bold = True
 
 
+
+# The sudoku board buttons are wrapped within 3 x 3 GridLayouts
 class SudokuBlock(GridLayout):
-    """A 3x3 GridLayout with custom background colour"""
-    pass
+    def __init__(self, **kwargs):
+        super(SudokuBlock, self).__init__(**kwargs)
 
 
-class SudokuBlockLight(SudokuBlock):
-    """A lighter/brighter version of SudokuBlock"""
-    pass
+## Test class to learn how to give a GridLayout button behaviours:
+# class GridButton(GridLayout, ButtonBehavior):
+#class GridButton(ButtonBehavior, GridLayout):
+#    pass
+
+    #def on_release(self):
+    #    print("plop")
 
 
-# RootWidget: The root of our widget tree: a sudoku board
-#             with a numerical keypad on the right
+# RootWidget: Sudoku board with a numerical keypad on the right
 class RootWidget(GridLayout):
     # note = False
     the_board = ListProperty()
+    active_number = NumericProperty()
+    active_number_string = StringProperty()
 
     def __init__(self, board: np.ndarray = None, debug: bool = None) -> None:
         super(RootWidget, self).__init__()
 
+        self.debug = debug
         self.board = board
+        self.build_the_board()
+        # self.add_gridbutton()
 
+        if self.debug:
+            print(self.board)
+
+    #def add_gridbutton(self):
+    #    gb = GridButton(cols=3, rows=3, size_hint=(0.1, 0.1))
+    #    with gb.canvas.before:
+    #        Color(1, 0.8, 0, 1)
+    #        self.rect = Rectangle(pos=gb.pos, size=gb.size)
+    #    gb.bind(on_release=functools.partial(print, "plip"))
+    #    gb.add_widget(Label(text="plop"))
+    #    self.add_widget(gb)
+
+
+
+    # Build the board widget tree:
+    def build_the_board(self):
         # Add a 3 by 3 GridLayout widget
         self.sudokuboard = GridLayout(cols=3, rows=3,
                                       orientation="lr-tb",
@@ -126,30 +183,35 @@ class RootWidget(GridLayout):
         # Add board buttons with text set to the corresponding
         # self.board[index] value. Also bind each button to the
         # 'update_boardbutton' function.
-
-        # Add board buttons and match indices...
         for n in range(3):
             for m in range(3):
-                sudoku_block = SudokuBlock(orientation='lr-tb')
+                sudoku_block = SudokuBlock(orientation='lr-tb')  # SudokuBlocks
                 self.sudokuboard.add_widget(sudoku_block)
                 for i in range(3):
                     for j in range(3):
-                        index = (n * 3 + i, m * 3 + j)
-                        board_button = BoardButton(disabled=False)
+                        index = (n * 3 + i, m * 3 + j)  # Index in the grand total GridLayout
+                        board_button = BoardButton(disabled=False)  # BoardButton
+                        # Bind the BoardButton to a function which updates the
+                        # label text to the active number:
                         board_button.bind(on_release=functools.partial(self.update_board_button, index=index))
-                        if self.board[index] in range(1, 10):
+
+                        # If the number at position 'index' is set (i.e. is not zero),
+                        # update the text of the corresponding BoardButton and disable it:
+                        if self.board[index]:
                             board_button.text = str(self.board[index])
                             board_button.disabled = True
-                        else:
-                            board_button.text = ""
+                        # Add the BoardButton to the SudokuBlock
                         sudoku_block.add_widget(board_button)
-
+        # Add the finalised sudoku board (GridLayout) to the RootWidget
         self.add_widget(self.sudokuboard)
 
-        if not self.board.all():
-            print(self.board)
-
-    def update_board_button(self, instance, index):
+    # Update the BoardButton text with the active number
+    def update_board_button(self, instance: BoardButton, index: tuple) -> None:
+        if self.debug:
+            print("index: ", index)
+            print("instance.text (pre):", instance.text)
+            print(f"self.board[{index}]:", self.board[index])
+            print(f"self.ids.active_number.text:", self.ids.active_number.text)
         if self.ids.active_number.text:
             self.board[index] = int(self.ids.active_number.text)
             instance.text = str(self.ids.active_number.text)
@@ -158,21 +220,10 @@ class RootWidget(GridLayout):
             self.board[index] = 0
             instance.text = ""
 
-        # print("instance:", instance)
-        # print("instance.text:", instance.text)
-        # print(index)
-        # print(self.board[index])
-        # print(self.ids.block11)
-        # block11 = self.ids.block11
-        # block11.add_widget(BoardButton(text='xx'))
-        # block11.add_widget(BoardButton(text='xy'))
-        # block11.add_widget(BoardButton(text='yy'))
-        # block41 = SudokuBlock()
-        # print(block41)
-        # print(self.board[index])
-        # self.ids['s05'].text = "99"
-        # print(type(self))
+        if self.debug:
+            print("instance.text (post):", instance.text)
 
+    # Quit the application (triggered by the Quit button)
     def do_quit(self, *args):
         print("See ya!", file=sys.stderr)
         raise SystemExit(0)
@@ -180,33 +231,36 @@ class RootWidget(GridLayout):
 
 class SudokuApp(App):
 
-    def __init__(self, board=np.zeros((9, 9), dtype=int), debug=None, **kwargs):
-        super(SudokuApp, self).__init__(**kwargs)
-        self.debug = debug  # gets passed on to RootWidget in self.build() below
-        self.board = board  # gets passed on to RootWidget in self.build() below
-        self.kv_file = "kv/sudoku.kv"
+    def __init__(self, board: np.ndarray = np.zeros((9, 9), dtype=int), debug: bool = None) -> None:
+        super(SudokuApp, self).__init__()
+        self.debug = debug  # gets passed on to RootWidget
+        self.board = board  # gets passed on to RootWidget
+        self.kv_file = "kv/sudoku.kv"  # the kv file is in a separate directory
 
     def build(self):
         self.root = RootWidget(self.board, self.debug)
         return self.root
 
     def on_start(self):
-        print("Hello : ) ")
+        pass
 
     def on_stop(self):
         print("Bye bye!")
 
 
 if __name__ == "__main__":
+    # Parse and get command line arguments
     args = parse_arguments()
+    debug = args.debug
+
+    # Load the sudoku board file, if provided on the command line
     if args.filename:
-        if args.debug:
-            sys_msg("args.filename:", args.filename.name)
-            print(type(args.filename))
+        if debug:
+            debug_msg("args.filename:", args.filename.name)
         try:
             board = np.loadtxt(args.filename.name, delimiter=",", dtype=int)
         except OSError as e:
             print(e)
-        SudokuApp(board, debug=args.debug).run()
+        SudokuApp(board, debug=debug).run()
     else:
-        SudokuApp(debug=args.debug).run()
+        SudokuApp(debug=debug).run()
