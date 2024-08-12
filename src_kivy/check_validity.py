0a8337fe3d1d980c0d91ac3f272d9e3f6b721f79
mier <mier@tanglewreck.mier.org>
2024-08-12 13:21:44 +0200

    Added three functions: main(), is_valid() and is_unique(), the latter of which replaces check_row() and ignores zeroes (i.e. blanks) in the array; is_valid() is essentially the old 'main()' and combines checks of rows and columns into the same for-loop

--- src_kivy/check_validity.py
+++ src_kivy/check_validity.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import numpy as np
 from utils import debug_msg, err_msg, sys_msg
 from utils import parse_arguments
@@ -17,33 +19,44 @@ a = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9],
               [9, 1, 2, 3, 4, 5, 6, 7, 8]])
 
 def check_row(row: np.ndarray) -> bool:
-    if len(set(row.flatten())) == len(row.flatten()):
-        return True
-    else:
-        return False
+    return len(set(row.flatten())) == len(row.flatten()):
 
 
-is_valid = True
-for row in range(9):
-    if not check_row(a[row]):
-        is_valid = False
-    else:
-        print(f"row {row} is valid: {a[row]}")
+def is_unique(array: np.ndarray) -> bool:
+    elements = [n for n in array if n != 0]
+    return len(elements) == len(set(elements))
 
-for col in range(9):
-    if not check_row(a[:, col]):
-        is_valid = False
-    else:
-        print(f"col {col} is valid: {a[col]}")
 
-for row in range(0, 9, 3):
-    for col in range(0, 9, 3):
-        print(a[row:row + 3, col:col +3].flatten())
-        if not check_row(a[row:row + 3, col:col +3].flatten()):
-            is_valid = False
+def is_valid(array: np.ndarray) -> bool:
+    # is_valid = True
+    # status_rows = status_cols = status_blocks = list()
 
-if debug:
-    if is_valid:
-        debug_msg("Grid is valid")
-    else:
-       debug_msg("Grid is not valid")
+    # Check rows and columns
+    for k in range(9):
+        if not is_unique(array[k, :]) or not is_unique(array[:, k]):
+            # return False
+            return_status = False
+        else:
+            print(f"row and/or column {k} is valid: {array[k]}")
+    
+    # Check blocks
+    for row in range(0, 9, 3):
+        for col in range(0, 9, 3):
+            print(array[row:row + 3, col:col +3].flatten())
+            if not check_row(array[row:row + 3, col:col +3].flatten()):
+                # return False
+                return_status = False
+            else:
+                print(f"block [{row}:{row + 3}, {col}:{col + 3}] is valid")
+    return return_status
+
+def main():
+    if debug:
+        if is_valid:
+            debug_msg("Grid is valid")
+        else:
+           debug_msg("Grid is not valid")
+
+
+if __name__ == "__main__":
+    main()
