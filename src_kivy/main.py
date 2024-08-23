dfe4af87aec921f95223b306eaae7984ce1ba2b2
mier <mier@tanglewreck.mier.org>
2024-08-23 13:57:26 +0200

    Moved kivy configuration stuff into a separate file (setup_kivy_config.py)

--- src_kivy/main.py
+++ src_kivy/main.py
@@ -36,7 +36,7 @@ __version__ = "2024-08-23"
 
 import os
 # NOTE: KIVY_HOME must be set *before* kivy is imported
-os.environ['KIVY_HOME'] = "./.kivy"
+os.environ['KIVY_HOME'] = f"{os.getcwd()}/.kivy"
 # Make kivy ignore command-line arguments 
 os.environ['KIVY_NO_ARGS'] = 'yes'
 
@@ -44,38 +44,13 @@ import numpy as np
 
 import kivy
 from kivy.config import Config
+from kivy.core.window import Window
 
 import solver
 from sudoku import SudokuApp
 from utils import debug_msg, err_msg, sys_msg, parse_arguments
 
-kivy.require("2.0.0")
-
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-# <KIVY CONFIG>
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-# Read the (local) configuration files and change stuff to our liking.
-# NOTE: Changes require a restart to take effect
-# NOTE: This code might probaly better be moved to its own file,
-#     which can be run when needed (+TODO-list)
-Config.read(f"{os.environ['HOME']}/Proj/sudoku/src_kivy/.kivy/config.ini")
-Config.set('kivy', 'exit_on_escape', 1)
-Config.set('kivy', 'log_enable', 1)
-Config.set('kivy', 'window_icon', "data/mier_347x437.jpg")
-Config.set('graphics', 'fullscreen', 0)
-Config.set('graphics', 'height', 600)
-Config.set('graphics', 'width', 1500)
-Config.set('graphics', 'resizable', 1)
-Config.set('graphics', 'position', 'custom')  # auto|custom
-Config.set('graphics', 'left', 100)         # ignored when 'position' == 'auto'
-Config.set('graphics', 'top', 100)          # ignored when 'position' == 'auto'
-Config.set('kivy', 'log_level', "warning")  # debug|info|warning|error|critical
-Config.write()
-
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-# </KIVY CONFIG>
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-
+# Window.size = (1500, 600)
 
 if __name__ == "__main__":
     # Parse and get command line arguments
---
+++ src_kivy/setup_kivy_config.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# ----------------------------------------
+# setup_kivy_config.py –
+# ----------------------------------------
+'''
+Create a local kivy config file
+
+'''
+
+__version__ = "2024-08-23"
+
+import os
+# NOTE: KIVY_HOME must be set *before* kivy is imported
+os.environ['KIVY_HOME'] = f"{os.getcwd()}/.kivy"
+# Make kivy ignore command-line arguments 
+os.environ['KIVY_NO_ARGS'] = 'yes'
+
+import kivy
+from kivy.config import Config
+kivy.require("2.0.0")
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# <KIVY CONFIG>
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# Read the (local) configuration files and change stuff to our liking.
+# NOTE: Changes require a restart to take effect
+# NOTE: This code might probaly better be moved to its own file,
+#     which can be run when needed (+TODO-list)
+Config.read(f"{os.environ['HOME']}/Proj/sudoku/src_kivy/.kivy/config.ini")
+Config.set('kivy', 'exit_on_escape', 1)
+Config.set('kivy', 'log_enable', 1)
+Config.set('kivy', 'window_icon', "data/mier_347x437.jpg")
+Config.set('graphics', 'fullscreen', 0)
+Config.set('graphics', 'height', 600)
+Config.set('graphics', 'width', 1500)
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'position', 'custom')  # auto|custom
+Config.set('graphics', 'left', 100)         # ignored when 'position' == 'auto'
+Config.set('graphics', 'top', 100)          # ignored when 'position' == 'auto'
+Config.set('kivy', 'log_level', "warning")  # debug|info|warning|error|critical
+Config.write()
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# </KIVY CONFIG>
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
