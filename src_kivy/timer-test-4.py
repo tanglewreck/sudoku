3e06ee48af1ea49a23a978b7a77da512a63827e8
mier.misa <mier.misa@>
2024-10-24 12:27:21 +0200

    command-line timer. Initial commit.

---
+++ src_kivy/timer-test-4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import datetime
+import functools
+import subprocess
+import time
+from utils import debug_msg, err_msg, sys_msg, parse_arguments
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    
